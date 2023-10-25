6825e05056f7c49c7453697636a343089e5951aa
sharekim <sksyag@naver.com>
2023-10-25 16:02:58 +0900

    컨텐츠 레이아웃 구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import Header from "@/layouts/header/Header";
+import Sidebar from "@/layouts/siderBar/Sidebar";
+import DrawingArea from "@/layouts/drawingArea/DrawingArea";
 
 export const metadata: Metadata = {
   title: "mini-canvas",
@@ -16,6 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
+        <div className="flex">
+          <Sidebar />
+          <DrawingArea />
+        </div>
       </body>
     </html>
   );
---
+++ src/layouts/drawingArea/DrawingArea.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DrawingArea = () => {
+  return <div className="m-2">DrawingArea</div>;
+};
+
+export default DrawingArea;
