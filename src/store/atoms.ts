c0692c17f95e39f196b47cb8349e84acad126c41
sharekim <sksyag@naver.com>
2023-11-01 20:53:07 +0900

    Recoil Persist 추가

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,6 +1,29 @@
-import { atom } from "recoil";
+import { AtomEffect, atom, useSetRecoilState } from "recoil";
+import { recoilPersist } from "recoil-persist";
+import { Shape } from "@/interface/interface";
+
+// next.js에서 사용하기 위해 ssr이 끝났는지를 확인하는 state이다.
+// 새로고침 시에 항상 default값인 false를 갖는다.
+const ssrCompletedState = atom({
+  key: "SsrCompleted",
+  default: false,
+});
+
+//useEffect에 쓰일 함수를 정의한다.
+export const useSsrComplectedState = () => {
+  const setSsrCompleted = useSetRecoilState(ssrCompletedState);
+  return () => setSsrCompleted(true);
+};
+
+const { persistAtom } = recoilPersist();
+
+//ssrCompletedState가 완료될 때까지 기다린 후, persistAtom을 반환한다.
+export const persistAtomEffect = <T>(param: Parameters<AtomEffect<T>>[0]) => {
+  param.getPromise(ssrCompletedState).then(() => persistAtom(param));
+};
 
 export const canvasNodeArrState = atom({
   key: "canvasNodes",
-  default: [],
+  default: new Array<Shape>(),
+  effects_UNSTABLE: [persistAtomEffect],
 });
