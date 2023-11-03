db9f27db3e9f8f6379b1da54c5a5688e2d4eb881
sharekim <sksyag@naver.com>
2023-11-03 16:07:48 +0900

    레이아웃 정리 (home)

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container } from "@mui/material";
+import Sidebar from "@/layouts/siderBar/Sidebar";
+import Canvas from "@/components/canvas/Canvas";
+
+const Home = () => {
+  const containerStyle = {
+    display: "flex",
+  };
+
+  return (
+    <Container disableGutters sx={containerStyle}>
+      <Sidebar />
+      <Canvas />
+    </Container>
+  );
+};
+
+export default Home;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,14 @@
 import "./globals.css";
 import type { Metadata } from "next";
 
-import { Container } from "@mui/material";
-
 import Header from "@/layouts/header/Header";
-import Sidebar from "@/layouts/siderBar/Sidebar";
-import DrawingArea from "@/layouts/drawingArea/DrawingArea";
+
 import RecoilRootProvider from "@/components/recoilRootProvider/RecoilRootProvider";
 import ToastProvider from "@/components/toastProvider/ToastProvider";
 
 export const metadata: Metadata = {
   title: "mini-canvas",
   description: "on canvas, design your own paper",
-};
-
-const containerStyle = {
-  display: "flex",
 };
 
 export default function RootLayout({
@@ -29,10 +22,7 @@ export default function RootLayout({
         <RecoilRootProvider>
           <ToastProvider />
           <Header />
-          <Container disableGutters sx={containerStyle}>
-            <Sidebar />
-            <DrawingArea />
-          </Container>
+          {children}
         </RecoilRootProvider>
       </body>
     </html>
